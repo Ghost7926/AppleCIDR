17e4c6422ed92a0be189162fd783799b19104c66
Ryan Voit <93566632+Ghost7926@users.noreply.github.com>
2024-07-04 20:35:40 -0700

    Update AppleCIDR.py
    accounts for x.x.x.x-x 
    accounts for domains
    accounts for spaces in file

--- AppleCIDR.py
+++ AppleCIDR.py
@@ -1,52 +1,62 @@
 import ipaddress
 import argparse
-import sys
 
-def extract_ips_from_range(ip_range):
+def extract_ips_from_range(item):
+    item = item.replace(" ", "")  # Remove spaces
+    item = item.strip()
     try:
-        ip_net = ipaddress.ip_network(ip_range, strict=False)
-        return list(map(str, ip_net.hosts()))
-    except ValueError:
-        return []
+        if '-' in item: # incase client list ip range as x.x.x.x-x
+            base_ip, range_part = item.split('-')
+            start_ip = ipaddress.IPv4Address(base_ip.strip())
+            end_ip = ipaddress.IPv4Address(base_ip.split('.')[0] + '.' + base_ip.split('.')[1] + '.' + base_ip.split('.')[2] + '.' + range_part.strip())
 
-def read_existing_ips(output_file):
-    existing_ips = set()
+            current_ip = start_ip
+            while current_ip <= end_ip:
+                yield str(current_ip)
+                current_ip += 1
+        elif '/' in item: # for CIDR notation ranges
+            ip_net = ipaddress.ip_network(item, strict=False)
+            for ip in ip_net.hosts():
+                yield str(ip)
+        else: # for domains
+            ip = ipaddress.ip_address(item)
+            yield str(ip)
+    except ValueError:
+        yield item
+
+def read_existing_items(output_file):
+    existing_items = set()
     try:
         with open(output_file, 'r') as f:
             for line in f:
-                ip = line.strip()
-                existing_ips.add(ip)
+                item = line.strip()
+                existing_items.add(item)
     except FileNotFoundError:
-        pass  # File does not exist initially, ignore
-    return existing_ips
+        pass
+    return existing_items
 
 def main(input_file, output_file):
-    existing_ips = read_existing_ips(output_file)
+    existing_items = read_existing_items(output_file)
 
     with open(input_file, 'r') as f:
         lines = f.readlines()
 
-    with open(output_file, 'a') as f_out:  # Open file in append mode
+    with open(output_file, 'a') as f_out:
         for line in lines:
-            line = line.strip()
-            ips = extract_ips_from_range(line)
-            for ip in ips:
-                if ip not in existing_ips:
-                    f_out.write(ip + '\n')
-                    existing_ips.add(ip)  # Add new IP to the set of existing IPs
+            items = extract_ips_from_range(line)
+            for item in items:
+                if item not in existing_items:
+                    f_out.write(item + '\n')
+                    existing_items.add(item)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="List out the IP Addresses in a file")
     parser.add_argument("filename", type=str, help="IP range file")
     args = parser.parse_args()
 
-    if len(args.filename.split()) > 1:
-        print("Error: Please provide only one file argument.")
-        print("If the file name has spaces, the program will believe it is multiple files.")
-        sys.exit(0)
-
     input_file = args.filename
-    output_file = 'listed_ips.txt'
+    output_file = 'listed_scope.txt'
+    
     print('Listing...')
     main(input_file, output_file)
     print('Done. Have a good day!')
